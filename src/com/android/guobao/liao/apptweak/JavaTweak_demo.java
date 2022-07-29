cd00fa9ea26c6e34fdd663c8660a1478552d1acd
lgb33026 <lgb33026@ly.com>
2022-07-29 12:42:02 +0800

    提交TweakMe3.0介绍文档

--- src/com/android/guobao/liao/apptweak/JavaTweak_demo.java
+++ src/com/android/guobao/liao/apptweak/JavaTweak_demo.java
@@ -6,8 +6,15 @@ import android.util.Log;
 @SuppressWarnings("unused")
 public class JavaTweak_demo { //替换方法所属的类，类名必须有统一的前缀【com.android.guobao.liao.apptweak.JavaTweak_***】
     static protected void loadDexFile(ClassLoader loader, String dex) {
-        //此函数内可以做一些初始化操作，比如加载native动态库，拦截android.jar包中的系统函数等等
-        JavaTweakBridge.writeToLogcat(Log.INFO, "nativeLoadLib: libname = libsodemo.so, handle = 0x%x", JavaTweakBridge.nativeLoadLib("libsodemo.so"));
+        //此函数内可以做一些初始化操作，比如加载native动态库，配置sotweak插件，拦截android.jar包中的系统函数等等
+        //插件配置不是必须的，如果重打包后app能正常运行就无需配置。
+        //如果重打包后app出现闪退现象，可以尝试依次放开下面的配置代码，在某些加固（比如梆梆）中会修复闪退现象，但不保证能修复所有闪退问题。
+        //JavaTweakBridge.setPluginFlags(JavaTweakBridge.PLUGIN_FLAG_DISABLE_SYSCALL);
+        //JavaTweakBridge.setPluginFlags(JavaTweakBridge.PLUGIN_FLAG_DISABLE_OPENAT);
+        //JavaTweakBridge.setPluginFlags(JavaTweakBridge.PLUGIN_FLAG_DISABLE_SYSCALL|JavaTweakBridge.PLUGIN_FLAG_DISABLE_OPENAT);
+
+        long handle = JavaTweakBridge.nativeLoadLib("libsodemo.so");
+        JavaTweakBridge.writeToLogcat(Log.INFO, "nativeLoadLib: libname = libsodemo.so, handle = 0x%x", handle);
 
         JavaTweakBridge.hookJavaMethod("javax.net.ssl.SSLContext", "init");
         JavaTweakBridge.hookJavaMethod("javax.crypto.Cipher", "getInstance(java.lang.String)"); //static
